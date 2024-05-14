8ab60984ef5de89e1f0311e87a1435ce124a8b85
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-14 16:42:00 +0900

    style : edit  SmallButton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import './styles/variables.css'
 import {LargeTitle, Link} from './components/TextComponents';
+import { SmallButton, TinyButton } from './components/Buttons';
 
 function App() {
   return (
     <div className="App">
       <LargeTitle text="안녕?"></LargeTitle>
       <Link text="카카오 드가자" link="https://www.kakaocorp.com/page/"></Link>
+      <SmallButton text="확인" isPurple="true"></SmallButton>
+      <TinyButton text="수정"></TinyButton>
     </div>
   );
 }
