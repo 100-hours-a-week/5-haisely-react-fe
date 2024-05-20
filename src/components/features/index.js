753af6380e5a63db10c4a5d1172688efa4d5d871
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-20 16:25:55 +0900

    feat: index.js for features

---
+++ src/components/features/index.js
@@ -0,0 +1,7 @@
+export * from './Board/Board';
+export * from './BoardInfo/BoardInfo';
+export * from './Comment/Comment';
+export * from './Dropdown/Dropdown';
+export * from './Modal/Modal';
+export * from './Profile/Profile';
+export * from './TextForm/TextForm'; 
