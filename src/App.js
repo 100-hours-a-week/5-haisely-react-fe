8848bf2d3276931a9f61d6af2c1ad81bc8b719b1
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-20 13:28:34 +0900

    feat: TextForm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {LargeTitle, Link} from './components/TextComponents';
 import { SmallButton, TinyButton } from './components/Buttons';
 import {BoardInfo} from './components/features/BoardInfo/BoardInfo';
 import {Profile } from './components/features/Profile/Profile';
+import {TextForm} from './components/features/TextForm/TextForm'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <TinyButton text="수정"></TinyButton>
       <BoardInfo text="조회수" num="123"></BoardInfo>
       <Profile name="이름" url="https://blog.kakaocdn.net/dn/tEMUl/btrDc6957nj/NwJoDw0EOapJNDSNRNZK8K/img.jpg" />
+      <TextForm text="이메일*" name="email" type="email" placeholder="이메일을 입력하세요" helpText="*이메일을 입력해주세요."></TextForm>
     </div>
   );
 }
--- src/components/features/TextForm/TextForm.jsx
+++ src/components/features/TextForm/TextForm.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./TextForm.module.css"
 import { Help, SmallTitle } from "../../TextComponents"
 
-export function TextForm({name, type, placeholder, helpText}){
-    return ( <div>
-        <label for={name}><SmallTitle text={name}/></label>
+export function TextForm({text, name, type, placeholder, helpText}){
+    return ( <div className={styles.form}>
+        <label for={name}><SmallTitle text={text}/></label>
             <input type={type} name={name} placeholder={placeholder}/>
             <Help text={helpText}/>
     </div>);
