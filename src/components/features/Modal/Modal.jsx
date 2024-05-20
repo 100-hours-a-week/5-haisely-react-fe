4a2fc30fbbbda54afa8eef6693b441575146cadd
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-20 13:59:37 +0900

    style : margin

--- src/components/features/Modal/Modal.jsx
+++ src/components/features/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Modal.module.css"
-import { MediumTitle, Content } from "../../TextComponents"
+import { MediumTitle } from "../../TextComponents"
 import { SmallButton } from "../../Buttons";
 
 export function Modal({text}){
